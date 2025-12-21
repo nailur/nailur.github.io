4bce420b94ac37179c9c25190c6faa9dafa8ddaa
SlenderPort791 <nailur.rohman@live.com>
2025-12-21 15:00:04 +0700

    update

--- api/harga-emas.js
+++ api/harga-emas.js
@@ -111,6 +111,8 @@ function parseLotusArchi(html) {
 		}
 	}
 
+	console.log(nodes);
+
 	const buyback = nodes[0].match(/Buyback Price\s*:\s*Rp\s*([\d.]+)/i)?.[1].replace(/\D/g, '');
 
 	const data = [];
