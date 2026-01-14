9b7786a7f86e5ce05b73089c41cb8b249e72c7c3
SlenderPort791 <nailur.rohman@live.com>
2026-01-14 13:40:45 +0700

    update

---
+++ api/db.js
@@ -0,0 +1,11 @@
+import mysql from 'mysql2/promise';
+
+// Reuse connection pool across function calls
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+export default pool;
