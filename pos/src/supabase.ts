1a6eb926f3bfbd1f976701b20df64ce430a2814e
SlenderPort791 <nailur.rohman@live.com>
2026-06-25 13:20:26 +0700

    delete

--- pos/src/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://cgudfsynurrrlkfzebgh.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNndWRmc3ludXJycmxrZnplYmdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyODIyNTQsImV4cCI6MjA5Nzg1ODI1NH0.8x-qh0BbPOGRk8wSB6LZvmLi4Uv1RU3_JOnJlKR4lkc';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
