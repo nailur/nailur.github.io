dd47e17350d5f690e8aa2e331bfa68a92f27a22d
SlenderPort791 <nailur.rohman@live.com>
2026-07-09 09:20:23 +0700

    fix: bump SW cache to v5 to propagate OneSignal tags

--- goldapp/sw.js
+++ goldapp/sw.js
@@ -1,11 +1,11 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
-const CACHE_NAME = 'goldapp-v4';
+const CACHE_NAME = 'goldapp-v5';
 const ASSETS_TO_CACHE = [
-    './index.html?v=4',
+    './index.html?v=5',
     './manifest.json',
-    './res/css/main.min.css?v=4',
-    './res/js/main.js?v=4'
+    './res/css/main.min.css?v=5',
+    './res/js/main.js?v=5'
 ];
 
 self.addEventListener('install', event => {
