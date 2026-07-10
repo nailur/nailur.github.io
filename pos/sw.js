1a3263350283a86ba66da35c46908c0bb27358d0
Nailur-PC <nailur.rohman@live.com>
2026-07-10 09:48:57 +0700

    chore: bump cache version to v3

--- pos/sw.js
+++ pos/sw.js
@@ -1,6 +1,6 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
-const CACHE_NAME = 'pos-cache-v2';
+const CACHE_NAME = 'pos-cache-v3';
 const urlsToCache = [
   './',
   './index.html',
