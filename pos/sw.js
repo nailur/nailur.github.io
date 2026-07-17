b7ba1fe2c61648a727133a03283a18beafa82c93
SlenderPort791 <nailur.rohman@live.com>
2026-07-18 00:32:02 +0700

    style: stack action buttons vertically in list view on mobile

--- pos/sw.js
+++ pos/sw.js
@@ -1,6 +1,6 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
-const CACHE_NAME = 'pos-cache-v9';
+const CACHE_NAME = 'pos-cache-v10';
 const urlsToCache = [
   './',
   './index.html',
