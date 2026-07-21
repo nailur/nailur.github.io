8046d03e5488772f0bce6f35a39fef5f9a4c29a7
Nailur-PC <nailur.rohman@live.com>
2026-07-22 00:44:30 +0700

    chore: hapus file script sementara dan tidak terpakai

--- check.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const code = fs.readFileSync('pos/js/management.js', 'utf8');
-let openBraces = 0;
-for (let i = 0; i < code.length; i++) {
-    if (code[i] === '{') openBraces++;
-    if (code[i] === '}') openBraces--;
-}
-console.log('Open braces: ' + openBraces);
--- hide_badges.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('pos/index.html', 'utf8');
-html = html.replace('id="op-user-info" class="user-badge hide-mobile"', 'id="op-user-info" class="user-badge hide-mobile hidden"');
-html = html.replace('id="sa-user-info" class="user-badge hide-mobile"', 'id="sa-user-info" class="user-badge hide-mobile hidden"');
-fs.writeFileSync('pos/index.html', html);
-console.log('Hidden user badges');
